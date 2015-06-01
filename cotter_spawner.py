69bdf55ea529c449688bfbfae07d4f371ca18f7f
Jon Ringuette <jonr667@uw.edu>
2015-06-01 12:30:45 -0700

    Added a bunch of comments

--- cotter_spawner.py
+++ cotter_spawner.py
@@ -11,6 +11,9 @@ from still.scheduler import Scheduler
 
 
 class SpawnerClass:
+    #
+    # Just create a class so I have a place to store some long lasting variables
+    #
     CommandLineArgs = ''
 
     def __init__(self):
@@ -20,26 +23,33 @@ class SpawnerClass:
 
 
 class MWAScheduler(Scheduler):
-
+    #
+    # Overload Scheduler class from still to be able to modify some functions
+    # 
     def __init__(self):
+        #
+        # This function is just here for fun and testing
+        #
         self.time_last_run = 0
 
     def ext_command_hook(self):
-        """
-        Overloading existing class function to customize what happens after each run
-        Things like sleeping when nothing to do or loading more obsid's in from ngas
-        and stuff such as that should go here
-        """
+        #
+        # Overloading existing class function to customize what happens after each run
+        # Things like sleeping when nothing to do or loading more obsid's in from ngas
+        # and stuff such as that should go here
+        #
         print("Ext_command_hook!")
         return
 
 
 class MWADataBaseInterface(DataBaseInterface):
-
+    #
+    # Overload DataBaseInterface class from still to be able to modify some functions
+    #
     def add_observation(self, obsnum, date, date_type, pol, filename, host, length=2 / 60. / 24, status='UV_POT'):
-        """
-        Overloading the existing class function to get MWA data in, though this might be generic enough to backport
-        """
+        #
+        # Overloading the existing class function to get MWA data in, though this might be generic enough to backport
+        #
         OBS = Observation(obsnum=obsnum, date=date, date_type=date_type, pol=0, status=status, length=length)
         print(OBS.obsnum)
         s = self.Session()
@@ -52,7 +62,7 @@ class MWADataBaseInterface(DataBaseInterface):
             print("Could not commit observation via add_observation.")
             exit(1)
 
-        # *JON* Not sure I want to add files here yet...
+        # Jon: Not sure I want to add files here yet...
         # self.add_file(obsnum, host, filename)  # todo test.
         # sys.stdout.flush()
         return obsnum
@@ -66,9 +76,12 @@ def sync_new_ops_from_ngas_to_still(SpawnerGlobal):
         pgconn = psycopg2.connect("dbname='test' user='test' host='localhost' password='testme'")
     except:
         print("I am unable to connect to the database")
+        exit(1)
     print("Probably connected")
-    cur_dict = pgconn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    cur = pgconn.cursor()
+    cur_dict = pgconn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)  # Get a sql cursor that supports returning data as a dict
+    cur = pgconn.cursor()  # Normal cursor, non-dict
+    # Get all the new OBS id's avaiable from the NGAS postgresql database and check against files that are currently in the still
+    # as well as files that are in the mwa_qc db and have thus already been processed
     cur.execute("""SELECT cast(substring(foreign_ngas_files.file_id, 1,10) AS bigint)
                    FROM
                       foreign_ngas_files
@@ -78,18 +91,20 @@ def sync_new_ops_from_ngas_to_still(SpawnerGlobal):
                    LIMIT 50""")
 
     rows = cur.fetchall()
-    unique_obsids = np.unique(rows)
+    unique_obsids = np.unique(rows)  # Lets just make sure we trim out all the extra obs id's we get from each having multiple files associated with it
 
-    for obsid in unique_obsids:
+    for obsid in unique_obsids:  # We now need to add all the files that are associated with each obs id to the db as well as the primary entry
         print("Adding obsid: %s") % obsid
-        SpawnerGlobal.db.add_observation(obsnum=obsid, date=obsid, date_type='GPS', pol=0, filename='none', host='none', length=0)
+        SpawnerGlobal.db.add_observation(obsnum=obsid, date=obsid, date_type='GPS', pol=0, filename='none', host='none', length=0)  # Add primary entry for obsnum
+        # Get all the files associated with each unique obsid, there is some redundency here but went for readability of logic over
+        # most effecient
         cur_dict.execute("""SELECT file_name, host_id, file_id, mount_point
                             FROM foreign_ngas_files
                               INNER JOIN foreign_ngas_disks
                               USING (disk_id)
                             WHERE cast(substring(foreign_ngas_files.file_id, 1,10) AS varchar(10)) = '%(myobsid)s' LIMIT 100""", {'myobsid': obsid})
         rows = cur_dict.fetchall()
-        for file_info in rows:
+        for file_info in rows:  # build the full path to each file based on ngas info and push each one into the db as a file of a unique obsid
             print(file_info)
             path = file_info['mount_point'] + '/' + file_info['file_name']
             print(path)
@@ -99,6 +114,10 @@ def sync_new_ops_from_ngas_to_still(SpawnerGlobal):
 
 
 def read_config_file(SpawnerGlobal, config_file, config_name='testing'):
+    #
+    # We will read the entire cnofig file here and push it into a class
+    # *This currently is a placeholder*
+    #
     if config_file is not None:
         config = configparser.ConfigParser()
         config_file = os.path.expanduser(config_file)
@@ -111,8 +130,12 @@ def read_config_file(SpawnerGlobal, config_file, config_name='testing'):
 
 
 def main(SpawnerGlobal, args):
+    #
     # Instantiate a still db instance from our superclass
-    SpawnerGlobal.db = MWADataBaseInterface(test=False, configfile='./cotter_still.cfg')
+    # Need to eventually call read_config_file first and then pass info and remove
+    # the reading of the config file from the DataBaseInterface class
+    #
+    SpawnerGlobal.db = MWADataBaseInterface(test=False, configfile='./cotter_still.cfg')                                       
     if args.init is True:   # See if we were told to initiate the database
         SpawnerGlobal.db.createdb()
         exit(0)
@@ -121,14 +144,17 @@ def main(SpawnerGlobal, args):
     except:
         print("We could not run a test on the database and are aborting.  Please check the DBI DB config")
         exit(1)
-    sync_new_ops_from_ngas_to_still(SpawnerGlobal)
-    myscheduler = MWAScheduler()
+    sync_new_ops_from_ngas_to_still(SpawnerGlobal)  # Lets get started and get a batch of new observations and push them into the db 
+    myscheduler = MWAScheduler()  # Init scheduler daemon
     MWAScheduler.init(myscheduler)
     # Will probably want to crank the sleep time up a bit in the future....
-    myscheduler.start(dbi=SpawnerGlobal.db, sleeptime=2)
+    myscheduler.start(dbi=SpawnerGlobal.db, sleeptime=2)  # Start the scheduler daemon
     return 0
 
-# Spawner = SpawnerClass
+
+#
+# Mostly placeholder stuff for reading in command line aruments
+#
 
 parser = argparse.ArgumentParser(description='Process MWA data.')
 
@@ -150,4 +176,3 @@ args, unknown = parser.parse_known_args()
 SpawnerGlobal.config_file = args.config_file
 print(SpawnerGlobal.config_file)
 main(SpawnerGlobal, args)
-
