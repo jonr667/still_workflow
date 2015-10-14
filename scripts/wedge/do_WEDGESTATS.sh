7fb9b4ec344c4eab058b3bc41593c08e2fdc5b28
Jon Ringuette <jonr667@uw.edu>
2015-10-14 09:47:45 -0700

    Added more error checking for postgres connection

--- scripts/wedge/do_WEDGESTATS.sh
+++ scripts/wedge/do_WEDGESTATS.sh
@@ -38,14 +38,30 @@ if [ $return_code = 0 ]; then
    ptsrc_wedge_y=`python -c "print '${WEDGESTATS}'.split()[10]"`
 
    previous_window=`psql -h $pg_qc_host $pg_qc_db -U $pg_qc_username -c "select count(*) from qs where obsid=${obs}" -t -A`
+   return_code=$?
+   if [ $return_code = 0 ]; then
+      echo "Could not connect to postgres db : $pg_qc_host"
+      exit 1
+   fi
    if [ $previous_window -eq 0 ]; then
       #this is the first time, do an insert
       echo "Creating new wedge record for $obs"
       psql -h $pg_qc_host $pg_qc_db -U $pg_qc_username -c "insert into qs (obsid,window_x,window_y,wedge_res_x,wedge_res_y,gal_wedge_x,gal_wedge_y,ptsrc_wedge_x,ptsrc_wedge_y,wedge_timestamp) values (${obs},${window_x},${window_y},${wedge_res_x},${wedge_res_y},${gal_wedge_x},${gal_wedge_y},${ptsrc_wedge_x},${ptsrc_wedge_y},current_timestamp)"
+      return_code=$?
+      if [ $return_code = 0 ]; then
+         echo "Could not connect to postgres db : $pg_qc_host"
+         exit 1
+      fi
+
    else
       #this is a redo, do an update
       echo "updating old wedge record for $obs"
       psql -h $pg_qc_host $pg_qc_db -U $pg_qc_username -c "update qs set (window_x,window_y,wedge_res_x,wedge_res_y,gal_wedge_x,gal_wedge_y,ptsrc_wedge_x,ptsrc_wedge_y,wedge_timestamp) = (${window_x},${window_y},${wedge_res_x},${wedge_res_y},${gal_wedge_x},${gal_wedge_y},${ptsrc_wedge_x},${ptsrc_wedge_y},current_timestamp) where obsid=${obs}"
+      return_code=$?
+      if [ $return_code = 0 ]; then
+         echo "Could not connect to postgres db : $pg_qc_host"
+         exit 1
+      fi
    fi    
 fi
 
